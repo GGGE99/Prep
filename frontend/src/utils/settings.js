f720d3afe53af86ab4fb7375fe74ffa71a869789
GGGE99 <42610254+GGGE99@users.noreply.github.com>
2021-01-04 14:02:02 +0100

    Delete settings.js

--- frontend/src/utils/settings.js
+++
@@ -1,7 +0,0 @@
-//const baseURL = "http://localhost:8080/jpareststarter/"
-const baseURL = "https://marcge.dk/tomcat/Eks/"
-
-
-export const loginURL = baseURL;
-
-export const jokeURL = baseURL + "api/jokes";
